a7675144f9aa0db69fb565818092a444c52bd7b2
nanairoaisu <b1018078@fun.ac.jp>
2020-09-07 00:31:36 +0900

    feat:感情選択から時間選択に遷移

--- MichisirubeApplication/app/src/main/java/com/example/michisirubeapplication/ui/naviEmotion/NaviEmotionFragment.kt
+++ MichisirubeApplication/app/src/main/java/com/example/michisirubeapplication/ui/naviEmotion/NaviEmotionFragment.kt
@@ -5,56 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.michisirubeapplication.R
+import kotlinx.android.synthetic.main.fragment_navi_emotion.view.*
+import kotlinx.android.synthetic.main.fragment_title.view.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [NaviEmotionFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class NaviEmotionFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_navi_emotion,container,false)
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+        view.btAnger.setOnClickListener{
+            findNavController().navigate(R.id.action_naviEmotion_to_naviTime)
         }
+        view.btJoy.setOnClickListener{
+            findNavController().navigate(R.id.action_naviEmotion_to_naviTime)
+        }
+        view.btEnjoyment.setOnClickListener{
+            findNavController().navigate(R.id.action_naviEmotion_to_naviTime)
+        }
+        view.btSad.setOnClickListener{
+            findNavController().navigate(R.id.action_naviEmotion_to_naviTime)
+        }
+        return view
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_navi_emotion, container, false)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment NaviEmotionFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            NaviEmotionFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
