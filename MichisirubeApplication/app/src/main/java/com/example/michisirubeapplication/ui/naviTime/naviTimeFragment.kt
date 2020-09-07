6b6cb183d60d10a5af494238ff19c7954b9b709e
nanairoaisu <b1018078@fun.ac.jp>
2020-09-07 14:38:31 +0900

    feat:時間のデータ取得した

--- MichisirubeApplication/app/src/main/java/com/example/michisirubeapplication/ui/naviTime/naviTimeFragment.kt
+++ MichisirubeApplication/app/src/main/java/com/example/michisirubeapplication/ui/naviTime/naviTimeFragment.kt
@@ -10,15 +10,19 @@ import androidx.fragment.app.DialogFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.michisirubeapplication.R
+import kotlinx.android.synthetic.main.fragment_navi_time.*
 import kotlinx.android.synthetic.main.fragment_navi_time.view.*
 
 
 class naviTimeFragment : DialogFragment() {
     private val args: naviTimeFragmentArgs by navArgs()
+    private var _selectHour = 0
+    private var _selectMinute = 5
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val timePicker = TimePicker(activity)
         timePicker.setIs24HourView(true)
+        //timePicker.currentMinute = 5
         return inflater.inflate(R.layout.fragment_navi_time, container, false)
     }
 
@@ -29,13 +33,26 @@ class naviTimeFragment : DialogFragment() {
         Toast.makeText(activity,args.emotion.toString(),Toast.LENGTH_SHORT)
             .show()
 
+        onTimeSet()
+
         view.btTimeSelect.setOnClickListener {
             //感情と時間を次に受け継ぐコード書く
-            val time = 0//時間ちゃんととってくる
+            onTimeSet()
+            val time: Int = _selectHour * 60 + _selectMinute//時間ちゃんととってくる
+            Toast.makeText(activity,time.toString(),Toast.LENGTH_SHORT)
+                .show()
+
             val action = naviTimeFragmentDirections.actionNaviTimeToNaviDestination(args.emotion,time)
             findNavController().navigate(action)
         }
 
+    }
+
+    fun onTimeSet() {
+        timePicker.setOnTimeChangedListener { timePicker,hour, minute ->
+            _selectHour = hour
+            _selectMinute = minute
+        }
     }
 
 }
